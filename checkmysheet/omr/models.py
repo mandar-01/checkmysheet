3b81a63c18e3d23c5d3c14388868a1aed2132c18
mandar-01 <bhaleraomandar01@gmail.com>
2017-12-28 16:25:02 +0530

    prepared model for image upload

--- checkmysheet/omr/models.py
+++ checkmysheet/omr/models.py
@@ -3,3 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	image = models.ImageField(null=True,blank=True)
--- checkmysheet/omr/views.py
+++ checkmysheet/omr/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
-	return render(request,"<html><body><h1>hello</h1></body></html>",{})
+	form = PostForm(request.POST or None,request.FILES or None)
+	context = {'status':'not working'}
+	if form.is_valid():
+		context['status'] = 'working'
+	return render(request,"index.html",context)
